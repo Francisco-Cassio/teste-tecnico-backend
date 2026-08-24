98ba3ab14264c41ec9af70a076e9970434422b66
Francisco-Cassio <franciscodecassiomourao@gmail.com>
2026-08-24 08:23:57 -0300

    perf: otimizar queries ORM e configurar paginação global

--- consultas/views.py
+++ consultas/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
 from django.db import transaction
+from datetime import date, timedelta
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from .models import Usuario, Especialista, Agenda, Horario
-from .serializers import UsuarioSerializer, EspecialistaSerializer, AgendaSerializer, HorarioSerializer
+from .models import Especialista, Agenda, Horario
+from .serializers import EspecialistaSerializer, AgendaSerializer, HorarioSerializer
 from .permissions import IsInternoOrReadOnly
 from .services import gerar_horarios_para_agenda
-from datetime import date, timedelta
 
 class EspecialistaViewSet(viewsets.ModelViewSet):
     queryset = Especialista.objects.all()
@@ -16,7 +15,7 @@ class EspecialistaViewSet(viewsets.ModelViewSet):
     permission_classes = [IsInternoOrReadOnly]
 
 class AgendaViewSet(viewsets.ModelViewSet):
-    queryset = Agenda.objects.all()
+    queryset = Agenda.objects.select_related('especialista').all()
     serializer_class = AgendaSerializer
     permission_classes = [IsInternoOrReadOnly]
 
@@ -31,11 +30,11 @@ class HorarioViewSet(viewsets.ModelViewSet):
     serializer_class = HorarioSerializer
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        queryset = super().get_queryset().select_related('agenda', 'agenda__especialista', 'cliente')
 
         especialista_id = self.request.query_params.get('especialista_id')
         data_consulta = self.request.query_params.get('data_consulta')
-        status = self.request.query_params.get('status')
+        status_param = self.request.query_params.get('status')
 
         if especialista_id:
             queryset = queryset.filter(agenda__especialista_id=especialista_id)
@@ -43,8 +42,8 @@ class HorarioViewSet(viewsets.ModelViewSet):
         if data_consulta:
             queryset = queryset.filter(data=data_consulta)
 
-        if status:
-            queryset = queryset.filter(status=status)
+        if status_param:
+            queryset = queryset.filter(status=status_param)
 
         return queryset
 
